df144884e5d5ea4753579f71ac38914ae3b2913f
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-03 18:56:41 -0700

    initial commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts 'Hello, programmer!'
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name='programmer')
+    puts "Hello, #{name}!"
+end
+
+def add (num_1, num_2)
+    num_1 + num_2
+end
+
+def halve num
+    return num / 2 unless num.class != Integer
+    nil
+end
+
+
+
